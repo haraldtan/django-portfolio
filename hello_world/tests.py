910ac7a0df705f85451e18fa734e6de69485bc41
haraldtan <56616203+haraldtan@users.noreply.github.com>
2019-11-20 18:30:16 +0800

    preparation for use with pytest

--- hello_world/tests.py
+++ hello_world/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
 # Create your tests here.
+class TestUrls:
+    def test_blog_url(self):
+        path = reverse('blog')
+        assert resolve(path).view_name == 'blog'
